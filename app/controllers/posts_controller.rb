051b1de00ec41ddf6c9a5c19a9c037c1476badba
Matt Bogen <zebogen@gmail.com>
2015-08-16 11:44:53 -0500

    Add view and routes for update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
@@ -24,6 +25,12 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
   end
 
   def destroy
